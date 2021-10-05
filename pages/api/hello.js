aad6da6e36913d2b09d18daa9c725217febe367a
Quentin Claes <quentin@cuustomer.com>
2021-10-05 15:58:39 +0200

    test

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,36 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
+var axios = require("axios").default;
+var options = {
+  method: 'POST',
+  url: 'https://www.warcraftlogs.com/oauth/token',
+  data: {
+    grant_type: 'client_credentials',
+    client_id: '94855a62-296d-4f59-a0df-047d707d0147 ',
+    client_secret: '0Oyyt39Qw5qCh8crYJWRGyPm2pJ4qcrVxZSitBbj',
+  }
+};
+
+
+
+  
+axios.request(options).then(function (response) {
+  console.log(response.data, options2);
+  var options2 = {
+  method: 'GET',
+  url: 'https://classic.warcraftlogs.com/api/v2/client/',
+    headers: { 'content-type': 'application/json', 'authorization': response.data.access_token }
+  };
+  
+  axios.request(options2).then(function (response) {
+  console.log(response.data);
+}).catch(function (error) {
+  console.error(error);
+});
+}).catch(function (error) {
+  console.error(error);
+});
+
   res.status(200).json({ name: 'John Doe' })
 }
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,10 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
+import useSSR from 'use-ssr'
 
 export default function Home() {
+
   return (
     <div className={styles.container}>
       <Head>
@@ -15,41 +17,9 @@ export default function Home() {
         <h1 className={styles.title}>
           Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
-
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h2>Documentation &rarr;</h2>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h2>Learn &rarr;</h2>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h2>Examples &rarr;</h2>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h2>Deploy &rarr;</h2>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <a href="https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=23BKWJ&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fdashboard&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800">
+Login to Fitbit
+</a>
       </main>
 
       <footer className={styles.footer}>
